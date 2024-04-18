7defc0d3c44d7e21436040595eafed53a1143e4c
abhi <goliabhinav.reddy2021@vitstudent.ac.in>
2024-04-18 21:02:19 +0530

    javascript_learniing

---
+++ javascript_learning/test.js
@@ -0,0 +1,5 @@
+console.log("Hitesh");
+// the environment is nodeJs
+//before people use to learn compile on browser
+// ig like i guess its made by on chromes v8 engine
+// node -v to see if its succeefully installed
