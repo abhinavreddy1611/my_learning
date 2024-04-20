a23217197d12670b8043910afcbd04b3b18f512d
abhi <goliabhinav.reddy2021@vitstudent.ac.in>
2024-04-20 11:52:31 +0530

    new changes

---
+++ javascript_learning/3datatypeconversion.js
@@ -0,0 +1,10 @@
+console.log(typeof NaN)
+// "33"=> 33
+//"33abs"=>NaN
+// type of NaN is number only
+// true=1 ; false=0; WHEN CONVERTED TO NUMBER
+// if you convert Number "23as "-> NaN
+console.log(Number("23 abc"))
+let isLoggedIn=1;
+let boolenisLoggedIn=Boolean(isLoggedIn);
+console.log(booleanisLoggedIn)
